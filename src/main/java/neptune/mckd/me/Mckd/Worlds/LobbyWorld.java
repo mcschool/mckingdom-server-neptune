06cab3b214d21ee0a15ea9c919a501c484e171c7
BuildTools <smatosan@gmail.com>
2019-09-23 16:40:29 +0900

    Adventure

--- src/main/java/neptune/mckd/me/Mckd/Worlds/LobbyWorld.java
+++ src/main/java/neptune/mckd/me/Mckd/Worlds/LobbyWorld.java
@@ -1,7 +1,11 @@
 package neptune.mckd.me.Mckd.Worlds;
 
 import neptune.mckd.me.Mckd.Neptune;
+import org.bukkit.GameMode;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 
 public class LobbyWorld implements Listener {
 
@@ -11,4 +15,15 @@ public class LobbyWorld implements Listener {
         this.plugin = plugin;
         this.plugin.getServer().getPluginManager().registerEvents(this, plugin);
     }
+
+    @EventHandler
+
+    public void onPlayerChangeWorld(PlayerChangedWorldEvent event){
+       Player player=event.getPlayer();
+        if (player.getWorld().getName().equals("lobby")){
+            player.setGameMode(GameMode.ADVENTURE);
+        }
+    }
 }
+
+
