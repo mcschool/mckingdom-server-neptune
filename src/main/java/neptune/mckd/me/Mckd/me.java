cca2b2cb8e52c2d3ff7c215b1092cef56a62cd99
Yuki-Shibanuma <smatosan@gmail.com>
2019-09-23 16:21:51 +0900

    first commit

---
+++ src/main/java/neptune/mckd/me/Mckd/me.java
@@ -0,0 +1,17 @@
+package neptune.mckd.me.Mckd;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class me extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
