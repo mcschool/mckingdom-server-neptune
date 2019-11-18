5da653a003e373c8789fef23482a6b662a69b3f8
Yuki-Shibanuma <smatosan@gmail.com>
2019-11-18 17:22:13 +0900

    a

--- src/main/java/me/mckd/neptune/Worlds/Villager/JoinCheckScheduler.java
+++ src/main/java/me/mckd/neptune/Worlds/Villager/JoinCheckScheduler.java
@@ -7,6 +7,7 @@ import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.LeatherArmorMeta;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -25,64 +26,33 @@ public class JoinCheckScheduler extends BukkitRunnable {
 
     @Override
     public void run() {
-        List<Player> players = this.world.getPlayers();
-        for (Player p: players) {
+        List<Player> players= this.world.getPlayers();
+        for (Player p:players){
             p.sendMessage(String.valueOf(this.count));
         }
-
-        if (this.count < 1) {
+        if (this.count< 1){
             int playerCount = players.size();
-            if (playerCount > 1) {
-                VillagerWorld v = new VillagerWorld(plugin);
-                for (int i=0; i < players.size(); i++) {
-                    Player p = players.get(i);
+            if (playerCount<1) {
+                for (int i = 0; i < players.size(); i++) {
                     Location location;
-                    // チームRED
-                    if (i % 4 == 0) {
-                        location = new Location(this.world, 0, 4, 0);
-                        this.setHelmet(p, Color.RED);
-                        p.teleport(location);
-                    }
-                    // チームBLUE
-                    if (i % 4 == 1) {
-                        location = new Location(this.world, 0, 4, 0);
-                        this.setHelmet(p, Color.BLUE);
-                        p.teleport(location);
-                    }
-                    // チームGREEN
-                    if (i % 4 == 2) {
-                        location = new Location(this.world, 0, 4, 0);
-                        this.setHelmet(p, Color.GREEN);
-                        p.teleport(location);
-                    }
-                    // チームYELLOW
-                    if (i % 4 == 3) {
-                        location = new Location(this.world, 0, 4, 0);
-                        this.setHelmet(p, Color.YELLOW);
-                        p.teleport(location);
-                    }
-                }
-            } else {
-                for (Player p : players) {
-                    p.sendMessage("まだ集まっていません...");
+                    Player p = players.get(0);
+                    location = new Location(this.world, -822, 201, -263);
+                    this.setHelmet(p, Color.RED);
+                    p.teleport(location);
                 }
             }
-            // スケジューラー終了
-            this.cancel();
+        } else {
+            for (Player p : players) {
+                p.sendMessage("Waiting for players");
+                p.sendMessage("Patience please");
+            }
+
         }
+        this.cancel();
     }
 
     public void setHelmet(Player player, Color color) {
-        // ヘルメット準備
-        ItemStack helmet = new ItemStack(Material.LEATHER_HELMET);
-        // ヘルメットのメタ情報準備
-        LeatherArmorMeta meta = (LeatherArmorMeta) helmet.getItemMeta();
-        // カラー設定
-        meta.setColor(color);
-        // ヘルメットにメタ情報セット
-        helmet.setItemMeta(meta);
-        //
-        player.getEquipment().setHelmet(helmet);
+        // 色付きのヘルメットを装備するプログラム
     }
 
 }
