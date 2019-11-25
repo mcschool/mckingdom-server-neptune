7dc343809b540b980abb8bde027b23028a33470a
Yuki-Shibanuma <smatosan@gmail.com>
2019-11-25 15:33:24 +0900

    villager

--- src/main/java/me/mckd/neptune/Worlds/Villager/VillagerWorld.java
+++ src/main/java/me/mckd/neptune/Worlds/Villager/VillagerWorld.java
@@ -11,6 +11,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
 
@@ -34,12 +35,15 @@ public class VillagerWorld implements Listener {
             return;
         }
         // スケジューラーを開始する
-        new SpawnDiamondScheduler(this.plugin, player.getWorld()).runTaskTimer(this.plugin, 0, 80);
+        // new SpawnDiamondScheduler(this.plugin, player.getWorld()).runTaskTimer(this.plugin, 0, 80);
+        new SpawnDiamondScheduler(this.plugin,player.getWorld()).runTaskTimer(this.plugin, 0 , 100);
 
         World world =player.getWorld();
         player.sendMessage(String.valueOf(world.getPlayers().size()));
-        if (world.getPlayers().size() == 1) {
-            new JoinCheckScheduler(this.plugin, world).runTaskTimer(this.plugin, 0, 20);
+        // n人以上集まったらゲームスタート
+        if (world.getPlayers().size() >= 2) {
+            // new JoinCheckScheduler(this.plugin, world).runTaskTimer(this.plugin, 0, 20);
+            // this.start();
         }
     }
 
@@ -65,12 +69,17 @@ public class VillagerWorld implements Listener {
         }
         e.setCancelled(true);
     }
+
+    // 11.25
     @EventHandler
-    public void onPlayerChangeWorld(PlayerChangedWorldEvent e){
-        Player player=e.getPlayer();
-        if(!e.getPlayer().getWorld().getName().equals(this.worldName)){
+    public void onPlayerInteractAtEntity(PlayerInteractAtEntityEvent e) {
+        Player player = e.getPlayer();
+        if (!player.getWorld().getName().equals("villager")) {
             return;
         }
-        new SpawnDiamondScheduler(this.plugin,player.getWorld()).runTaskTimer(this.plugin, 0 , 100);
+
+        Entity entity = e.getRightClicked();
+        player.sendMessage(entity.getType().toString());
     }
+
 }
