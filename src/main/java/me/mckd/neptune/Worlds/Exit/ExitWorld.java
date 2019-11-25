803c1ee6f3e9a591751cb14da07735ade47748fb
Yuki-Shibanuma <smatosan@gmail.com>
2019-11-25 15:24:04 +0900

    exit

--- src/main/java/me/mckd/neptune/Worlds/Exit/ExitWorld.java
+++ src/main/java/me/mckd/neptune/Worlds/Exit/ExitWorld.java
@@ -1,16 +1,16 @@
 package me.mckd.neptune.Worlds.Exit;
 
 import me.mckd.neptune.Neptune;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 
 import java.util.List;
 
@@ -41,7 +41,7 @@ public class ExitWorld implements Listener {
             p.sendMessage("今"+playerNum + "人になった？");
         }
         // n人以上集まったらゲームを開始する
-        if (playerCount >= 1) {
+        if (playerCount >= 2) {
             this.start();
         }
     }
@@ -55,11 +55,20 @@ public class ExitWorld implements Listener {
         if (e.getBlock().getType() == Material.LOG_2) {
             e.setCancelled(true);
             Block block = e.getBlock();
+            World world = block.getWorld();
             block.setType(Material.REDSTONE_BLOCK);
             this.count--;
             List<Player> players = e.getBlock().getWorld().getPlayers();
             for (Player p: players) {
-                p.sendTitle("あと" + String.valueOf(this.count) + "個", "", 20, 20, 20);
+                if (this.count == 0) {
+                    p.sendTitle("全部壊した！", "", 20, 20, 20);
+                } else {
+                    p.sendTitle("あと" + String.valueOf(this.count) + "個", "", 20, 20, 20);
+                }
+            }
+            if (this.count == 0) {
+                world.getBlockAt(new Location(world, -1026, 4, -1141)).setType(Material.AIR);
+                world.getBlockAt(new Location(world, -1026, 5, -1141)).setType(Material.AIR);
             }
         }
     }
@@ -77,8 +86,9 @@ public class ExitWorld implements Listener {
         world.getBlockAt(new Location(world, -1030, 5, -1090)).setType(Material.LOG_2);
         world.getBlockAt(new Location(world, -1052, 4, -1086)).setType(Material.LOG_2);
 
-        world.getBlockAt(new Location(world, -1025, 4, -1140)).setType(Material.REDSTONE_BLOCK);
-        world.getBlockAt(new Location(world, -1025, 5, -1140)).setType(Material.REDSTONE_BLOCK);
+        // 鉄のフェンスを元に戻す
+        world.getBlockAt(new Location(world, -1026, 4, -1141)).setType(Material.IRON_FENCE);
+        world.getBlockAt(new Location(world, -1026, 5, -1141)).setType(Material.IRON_FENCE);
         // レッドストーンランプカウントリセット
         this.count = 5;
 
@@ -95,4 +105,43 @@ public class ExitWorld implements Listener {
             }
         }
     }
+
+    @EventHandler
+    public void onEntityDamageByEntity(EntityDamageByEntityEvent e) {
+        if(!e.getEntity().getWorld().getName().equals("exit")) {
+            return;
+        }
+        // ダメージを受けたエンティティがプレーヤーの場合ろうやに入れる
+        if (e.getEntity() instanceof Player) {
+            Player player = (Player) e.getEntity();
+            // 牢屋の座標
+            Location location = new Location(e.getEntity().getWorld(), -994, 31, -1138);
+            // テレポートさせる
+            player.teleport(location);
+        }
+    }
+
+
+
+    // ブロック or 空中をクリック・右クリックした時の処理
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent e) {
+        Player player = e.getPlayer();
+        if (!player.getWorld().getName().equals("arrow")) {
+            return;
+        }
+        // ブロックを右クリック
+        if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+            // エンダーチェストを右クリック
+            if (e.getClickedBlock().getType() == Material.ENDER_CHEST) {
+                World world = player.getWorld();
+                List<Player> players = world.getPlayers();
+                for (Player p: players) {
+                    p.sendTitle("Bの勝ち〜〜〜！", "", 40, 40, 20);
+                    p.performCommand("mvtp lobby");
+                }
+            }
+        }
+    }
+
 }
