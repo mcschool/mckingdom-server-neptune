d669f8dc3ce9424b176b53fc7a9550bc44911a6c
Yuki-Shibanuma <smatosan@gmail.com>
2019-11-25 14:43:28 +0900

    add item for lobby

--- src/main/java/me/mckd/neptune/Worlds/Exit/ExitWorld.java
+++ src/main/java/me/mckd/neptune/Worlds/Exit/ExitWorld.java
@@ -1,6 +1,10 @@
 package me.mckd.neptune.Worlds.Exit;
 
 import me.mckd.neptune.Neptune;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -27,13 +31,17 @@ public class ExitWorld implements Listener {
         if (!player.getWorld().getName().equals(this.worldName)) {
             return;
         }
+
+        // 今ワールドにいるプレーヤーの人数を調べる
         List<Player> players = player.getWorld().getPlayers();
         int playerCount = players.size();
         String playerNum = String.valueOf(playerCount);
         for (Player p:players) {
             p.sendMessage("今"+playerNum + "人になった？");
         }
-
+        if (playerCount >= 1) {
+            this.start();
+        }
     }
 
     @EventHandler
@@ -43,11 +51,29 @@ public class ExitWorld implements Listener {
         if(!player.getWorld().getName().equals("exit")) {
             return;
         }
-        Block block = e.getBlock();
-        player.sendMessage("ブロック壊した"+ block.getType().toString() );
-
-
+        if (e.getBlock().getType() == Material.LOG_2) {
+            Block block = e.getBlock();
+            Location location = block.getLocation();
+            location.setY(location.getY() + 1);
+            location.getBlock();
+            player.sendMessage(location.getBlock().getType().toString());
+        }
     }
 
-
+    private void start() {
+        World world = Bukkit.getWorld("exit");
+        List<Player> players = world.getPlayers();
+        for (int i = 0; i < players.size(); i++) {
+            Player player = players.get(i);
+            if (i == 0) {
+                player.setDisplayName("A");
+                Location location = new Location(world, -1026, 5, -1138);
+                player.teleport(location);
+            } else {
+                player.setDisplayName("B");
+                Location location = new Location(world, -1024, 5, -1089);
+                player.teleport(location);
+            }
+        }
+    }
 }
