bc28c0bcf3dbf2b594e7d98847059ebabfea3b06
BuildTools <smatosan@gmail.com>
2019-12-02 17:11:42 +0900

    Update 10

--- src/main/java/me/mckd/neptune/Worlds/Villager/SpawnIronScheduler.java
+++ src/main/java/me/mckd/neptune/Worlds/Villager/SpawnIronScheduler.java
@@ -12,19 +12,29 @@ public class SpawnIronScheduler extends BukkitRunnable {
     World world;
     int count = 0;
 
-    public SpawnIronScheduler(Neptune plugin,World world){
-        this.plugin=plugin;
-        this.world=world;
+    public SpawnIronScheduler(Neptune plugin, World world) {
+        this.plugin = plugin;
+        this.world = world;
     }
 
     @Override
-    public void run(){
-        Location location=new Location(this.world,-822,201,-355);
-        this.world.dropItemNaturally(location,new ItemStack(Material.IRON_INGOT));
+    public void run() {
+        Location location = new Location(this.world, -822, 201, -355);
+        this.world.dropItemNaturally(location, new ItemStack(Material.IRON_INGOT));
 
-        if(this.count>500){
+
+        Location location2 = new Location(this.world, -822, 201, 259);
+        this.world.dropItemNaturally(location2, new ItemStack(Material.IRON_INGOT));
+
+
+        Location location3 = new Location(this.world, -870, 201, -307);
+        this.world.dropItemNaturally(location3, new ItemStack(Material.IRON_INGOT));
+
+        Location location4 = new Location(this.world, -774, 201, -307);
+        this.world.dropItemNaturally(location4, new ItemStack(Material.IRON_INGOT));
+
+        if (this.count > 500) {
             this.cancel();
         }
     }
-
 }
