753a83c6635a51c462242fce0cc09cdfe32f843b
BuildTools <smatosan@gmail.com>
2019-12-16 17:45:42 +0900

    v

--- src/main/java/me/mckd/neptune/Worlds/Villager/VillagerWorld.java
+++ src/main/java/me/mckd/neptune/Worlds/Villager/VillagerWorld.java
@@ -6,10 +6,12 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.event.inventory.InventoryType;
@@ -25,7 +27,13 @@ import java.util.List;
 public class VillagerWorld implements Listener {
 
     Neptune plugin;
-    String worldName = "villager";
+    String worldName = "Villager";
+    String gameStatus = "preparation";
+
+    Boolean isRedTeamVillagerDied = false;
+    Boolean isBlueTeamVillagerDied = false;
+    Boolean isGreenTeamVillagerDied = false;
+    Boolean isYelowTeamVillagerDied = false;
 
     public VillagerWorld(Neptune plugin) {
         this.plugin = plugin;
@@ -96,7 +104,7 @@ public class VillagerWorld implements Listener {
         inv.clear();
 
         inv.setItem(0, this.setItemForGui(Material.IRON_AXE, "10", 1));
-        inv.setItem(1, this.setItemForGui(Material.DIRT, "1",16));
+        inv.setItem(1, this.setItemForGui(Material.DIRT, "1", 16));
         player.openInventory(inv);
     }
 
@@ -108,7 +116,51 @@ public class VillagerWorld implements Listener {
         itemStack.setItemMeta(itemMeta);
         return itemStack;
     }
+
+    @EventHandler
+    public void onBlockBreak(BlockBreakEvent e) {
+        /*
+        Player player = e.getPlayer();
+        if (!player.getWorld().getName().equals("villager")) {
+            return;
+        }
+        if (e.getBlock().getType() == Material.REDSTONE_BLOCK) {
+            this.gameFinish();
+            return;
+        }
+        if (e.getBlock().getType() == Material.ENCHANTMENT_TABLE) {
+            this.gameStart();
+        }
+        */
+    }
+    public void gameStart(){
+        /*
+        World world=Bukkit.getWorld("villager");
+        this.gameStatus="start";
+        this.isBlueTeamVillagerDied=false;
+        this.isGreenTeamVillagerDied=false;
+        this.isRedTeamVillagerDied=false;
+        this.isYelowTeamVillagerDied=false;
+
+        Location locationRed=new Location(world, 100, 100, 100);
+        Villager villagerRed=(Villager) world.spawnEntity(locationRed, EntityType.Villager);
+
+        List<Player> players=world.getPlayers();
+        for (int i=0; 1<players.size();i++) {
+            Player p = players.get(i);
+            if (i % 4 == 0) ;
+
+            Location location = new Location(world, 100, 100, 100);
+            p.teleport(location);
+            
+        }
+        */
+
+    }
+
 }
+
+
 
 
 
